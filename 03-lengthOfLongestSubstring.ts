a8f059b97b9f1a5af5c80ee1c4a94024933b92fe
x y w <xyw283@gmail.com>
2023-03-07 22:33:04 +0800

    03-lengthOfLongestSubstring

--- 03-lengthOfLongestSubstring.ts
+++ 03-lengthOfLongestSubstring.ts
@@ -1,12 +1,21 @@
 function lengthOfLongestSubstring(s: string): number {
 
-  let last:Array<string> = [];
-  for(let i = 0; i < s.length; i++){
+  const map = new Map<string, number>();
+  let maxLength = 0;
+  let left = 0;
+  for (let i = 0; i < s.length; i++) {
+    const rightChar = s[i];
 
+    if (map.has(rightChar) && map.get(rightChar)! >= left) {
+      left = map.get(rightChar)! + 1;
+    }
+    map.set(rightChar, i);
 
+    const currentLength = i - left + 1;
+    maxLength = Math.max(currentLength, maxLength);
   }
 
-  return 0;
+  return maxLength;
 };
 
 console.log(lengthOfLongestSubstring("dvdf"));
