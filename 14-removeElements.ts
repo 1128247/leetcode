f01e8bd0195e15a7ba636e2ef763fe1ad24bc137
xyw <xyw283@gmail.com>
2023-03-13 13:30:32 +0800

    移除链表元素

---
+++ 14-removeElements.ts
@@ -0,0 +1,28 @@
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+
+  let current = head;
+  while (current) {
+    if (current.next?.val === val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next
+    }
+  }
+
+  return head?.val === val ? head.next : head;
+};
+
+// removeElements(new ListNode(1, new ListNode(2, new ListNode(6, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))))), 6);
+// removeElements(new ListNode(7, new ListNode(7, new ListNode(7 ))), 7);
+console.log(removeElements(new ListNode(1, new ListNode(2)), 1));
+export { };
