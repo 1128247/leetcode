0afc38f7bc22c231121c0b5aa81acf928005607d
xyw <xyw283@gmail.com>
2023-03-14 14:59:46 +0800

    相同的树

---
+++ 21-isSameTree.ts
@@ -0,0 +1,9 @@
+import { TreeNode } from "./TreeNode";
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if(p === null && q === null) return true;
+  if(p === null) return false;
+  if(q === null) return false;
+  if(p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
